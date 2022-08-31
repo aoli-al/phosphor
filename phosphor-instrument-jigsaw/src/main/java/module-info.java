71ebcd54cfd5218392f2d96e3403cf25b31c282a
Ao Li <leeleo3x@gmail.com>
2022-08-31 10:59:55 -0500

    Add more opens.

--- phosphor-instrument-jigsaw/src/main/java/module-info.java
+++ phosphor-instrument-jigsaw/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module edu.columbia.cs.psl.jigsaw.phosphor.instrumenter {
     opens edu.columbia.cs.psl.phosphor.instrumenter;
     opens edu.columbia.cs.psl.phosphor.struct.harmony.util;
     opens edu.columbia.cs.psl.phosphor.org.objectweb.asm;
+    opens edu.columbia.cs.psl.phosphor.org.objectweb.asm.tree;
     requires jdk.jlink;
     requires java.instrument;
 }
